0e2e7a4b47fec524bfab67f7732881cb45c22ac0
Igor Pereira Lobo <igorplobo@gmail.com>
2024-07-11 09:21:02 -0300

    grafico funcionando
    Grafico funcionando e filtrando corretamente

--- src/app/relatorio/relatorio.component.ts
+++ src/app/relatorio/relatorio.component.ts
@@ -1,42 +1,102 @@
+import { DataService } from './../services/data.service';
 import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
-import { DataService } from '../services/data.service';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Chart, registerables } from 'chart.js';
+Chart.register(...registerables);
+
 
 @Component({
   selector: 'app-relatorio',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,ReactiveFormsModule],
   templateUrl: './relatorio.component.html',
   styleUrl: './relatorio.component.css'
 })
 export class RelatorioComponent implements OnInit {
+  filtroForm: FormGroup;
   orgaos: any[] = [];
-  manifestacaoTipos: any[] = [];
-  assuntos: any[]=[];
-  subassuntos: any[]=[];
-  tramites: any[]=[];
+  tipos: any[] = [];
+  assuntos: any[] = [];
+  subassuntos: any[] = [];
+  tramites: any[] = [];
+  graficoPainel1: any;
+  totalRegistros: number = 0;
 
-
-  constructor(private  dataServices: DataService) { }
+  constructor(private fb: FormBuilder, private dataService: DataService) {
+    this.filtroForm = this.fb.group({
+      dataInicial: [''],
+      dataFinal: [''],
+      orgao: [''],
+      tipo: [''],
+      assunto: [''],
+      subassunto: [''],
+      tramite: ['']
+    });
+  }
 
   ngOnInit(): void {
-    this.dataServices.getManifestacaoTipos().subscribe(data => {
-      this.manifestacaoTipos = data;
-    });
-    this.dataServices.getAssuntos().subscribe(data => {
-      this.assuntos = data;
+    this.carregarCombosIniciais();
+  }
 
-    });
-    this.dataServices.getSubassuntos().subscribe(data => {
-      this.subassuntos= data;
-    });
-    this.dataServices.getTramites().subscribe(data => {
-      this.tramites= data;
-    });
-    this.dataServices.getOrgaos().subscribe(data => {
-      this.orgaos= data;
-    });
+  carregarCombosIniciais() {
+    this.dataService.getOrgaos().subscribe(data => this.orgaos = data);
+    this.dataService.getManifestacaoTipos().subscribe(data => this.tipos = data);
+    this.dataService.getAssuntos().subscribe(data => this.assuntos = data);
+    this.dataService.getTramites().subscribe(data => this.tramites = data);
+  }
 
+  onAssuntoChange() {
+    const idAssunto = this.filtroForm.get('assunto')?.value;
+    this.dataService.getSubassuntos(idAssunto).subscribe(data => this.subassuntos = data);
+  }
+
+  onSubmit() {
+    if (this.filtroForm.valid) {
+      this.carregarGraficos();
+    }
+  }
+
+  carregarGraficos() {
+    const filtros = this.filtroForm.value;
+    this.dataService.getDadosPainel(1, filtros).subscribe(data => {
+      const tipos = data.map((item: any) => item.ds_manifestacao_tipo);
+      const nums = data.map((item: any) => item.num);
+
+      const totalRegistros = nums.reduce((a: number, b: number) => a + Number(b), 0);
+      this.totalRegistros = totalRegistros.toLocaleString('pt-BR');
+
+      const ctx = document.getElementById('painel1') as HTMLCanvasElement;
+      if (this.graficoPainel1) this.graficoPainel1.destroy();
+
+      this.graficoPainel1 = new Chart(ctx, {
+        type: 'bar',
+        data: {
+          labels: tipos,
+          datasets: [{
+            label: 'Quantidade de Manifestações',
+            data: nums,
+            backgroundColor: 'rgba(54, 162, 235, 0.2)',
+            borderColor: 'rgba(54, 162, 235, 1)',
+            borderWidth: 1
+          }]
+        },
+        options: {
+          maintainAspectRatio: false,
+          scales: {
+            y: {
+              beginAtZero: true
+            }
+          },
+          plugins: {
+            legend: {
+              display: false
+            }
+          }
+        }
+      });
+    });
   }
 }
+
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,43 +1,41 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
-  private apiOrgao = 'https://www.ouvidoriageral.go.gov.br/api/api/allorgaos';
-  private apiUrlTipos = 'https://www.ouvidoriageral.go.gov.br/api/api/manifestacao-tipos';
-  private apiUrlAssuntos = 'https://www.ouvidoriageral.go.gov.br/api/api/assuntos';
-  private apiSubassuntos = 'https://www.ouvidoriageral.go.gov.br/api/api/subassuntos';
-  private apiTramites = 'https://www.ouvidoriageral.go.gov.br/api/api/tipos-tramites';
-
-  private apiPainelI = 'https://www.ouvidoriageral.go.gov.br/api/api/relatorios/dados-painel?painel=1';
-
-
+  private apiUrl = 'https://www.ouvidoriageral.go.gov.br/api/api';
 
   constructor(private http: HttpClient) { }
 
+  getOrgaos(): Observable<any> {
+    return this.http.get(`${this.apiUrl}/allorgaos`);
+  }
+
   getManifestacaoTipos(): Observable<any> {
-    return this.http.get<any>(this.apiUrlTipos);
+    return this.http.get(`${this.apiUrl}/manifestacao-tipos`);
   }
 
   getAssuntos(): Observable<any> {
-    return this.http.get<any>(this.apiUrlAssuntos);
+    return this.http.get(`${this.apiUrl}/assuntos`);
   }
 
-  getSubassuntos(): Observable<any> {
-    return this.http.get<any>(this.apiSubassuntos);
+  getSubassuntos(idAssunto: string): Observable<any> {
+    const params = new HttpParams().set('id_assunto', idAssunto);
+    return this.http.get(`${this.apiUrl}/subassuntos`, { params });
   }
 
   getTramites(): Observable<any> {
-    return this.http.get<any>(this.apiTramites);
+    return this.http.get(`${this.apiUrl}/tipos-tramites`);
   }
 
-  getOrgaos(): Observable<any> {
-    return this.http.get<any>(this.apiOrgao);
-  }
-  getPainelI(): Observable<any>{
-    return this.http.get<any>(this.apiPainelI);
+  getDadosPainel(painel: number, filtros: any): Observable<any> {
+    let params = new HttpParams();
+    Object.keys(filtros).forEach(key => {
+      params = params.set(key, filtros[key]);
+    });
+    return this.http.get(`${this.apiUrl}/relatorios/dados-painel?painel=${painel}`, { params });
   }
 }
